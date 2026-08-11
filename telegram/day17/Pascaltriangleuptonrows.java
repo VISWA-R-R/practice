e6fd296844ea502fc4cf1b3aecec48a1f90d7b35
Viswa R R <viswaoff03@gmail.com>
2026-08-11 19:41:59 +0530

    Pascaltriangleuptonrows

---
+++ telegram/day17/Pascaltriangleuptonrows.java
@@ -0,0 +1,15 @@
+package telegram.day17;
+
+public class Pascaltriangleuptonrows {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=0;i<n;i++){
+            int number=1;
+            for(int j=0;j<=i;j++){
+                System.out.print(number+" ");
+                number=number*(i-j)/(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
