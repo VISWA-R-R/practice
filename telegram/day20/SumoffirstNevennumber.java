dcdd70cb258df917805d5185816de5ecb3efad84
Viswa R R <viswaoff03@gmail.com>
2026-08-14 15:51:08 +0530

    SumoffirstNevennumber

---
+++ telegram/day20/SumoffirstNevennumber.java
@@ -0,0 +1,12 @@
+package telegram.day20;
+
+public class SumoffirstNevennumber {
+    public static void main(String[] args) {
+        int n=4;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+(2*i);
+        }
+        System.out.println(sum);
+    }
+}
