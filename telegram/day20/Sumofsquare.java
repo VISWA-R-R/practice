1e17eb19c40d753620a31c6476db83aa26bddcf4
Viswa R R <viswaoff03@gmail.com>
2026-08-14 16:01:39 +0530

    Sumofsquare

---
+++ telegram/day20/Sumofsquare.java
@@ -0,0 +1,11 @@
+package telegram.day20;
+
+public class Sumofsquare {
+    public static void main(String[] args) {
+        int n=4;
+        int square=n*n;
+        // System.out.println(square);
+        int sum=square+n;
+        System.out.println(sum);
+    }
+}
