ff35c1865a56c6e13a204d63831a01f5df4ca91e
Viswa R R <viswaoff03@gmail.com>
2026-08-16 09:24:49 +0530

    Trianglepatternforstars

---
+++ telegram/day22/Trianglepatternforstars.java
@@ -0,0 +1,13 @@
+package telegram.day22;
+
+public class Trianglepatternforstars {
+    public static void main(String[] args) {
+        int height=4;
+        for(int i=1;i<=height;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
