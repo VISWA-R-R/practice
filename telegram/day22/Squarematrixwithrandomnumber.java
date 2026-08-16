9c3f3fb213c60526aaa010e84f749a35cf573dab
Viswa R R <viswaoff03@gmail.com>
2026-08-16 09:31:45 +0530

    Squarematrixwithrandomnumber

---
+++ telegram/day22/Squarematrixwithrandomnumber.java
@@ -0,0 +1,15 @@
+package telegram.day22;
+
+public class Squarematrixwithrandomnumber {
+    public static void main(String[] args) {
+        int size=3;
+        int matrix[][]=new int[size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                System.out.print((matrix[i][j]=(int)(Math.random()*10))+" ");
+                // System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
