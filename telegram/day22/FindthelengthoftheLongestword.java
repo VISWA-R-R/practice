c3cd1358b3cfa19cd7d8406342cab4615bd89038
Viswa R R <viswaoff03@gmail.com>
2026-08-16 09:40:31 +0530

    FindthelengthoftheLongestword

---
+++ telegram/day22/FindthelengthoftheLongestword.java
@@ -0,0 +1,16 @@
+package telegram.day22;
+
+public class FindthelengthoftheLongestword {
+    public static void main(String[] args) {
+        String str="Find the longest word";
+        String str2[]=str.split(" ");
+        int maxlength=Integer.MIN_VALUE;
+
+        for(int i=0;i<str2.length;i++){
+            if(str2[i].length()>maxlength){
+                maxlength=str2[i].length();
+            }
+        }
+        System.out.println(maxlength);
+    }
+}
