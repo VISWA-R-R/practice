ded3e5a65fa12e1416204e9c02790ff90a988ec0
Viswa R R <viswaoff03@gmail.com>
2026-08-18 17:05:29 +0530

    Rightangletriangle

---
+++ telegram/day24/Rightangletriangle.java
@@ -0,0 +1,13 @@
+package telegram.day24;
+
+public class Rightangletriangle {
+    public static void main(String[] args) {
+        int input=4;
+        for(int i=0;i<input;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
