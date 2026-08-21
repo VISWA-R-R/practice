7edfc89c7beafb586ff2fc592ff3f19522556680
Viswa R R <viswaoff03@gmail.com>
2026-08-21 17:55:20 +0530

    Pyramidpatternofincreasingstars

---
+++ telegram/day27/Pyramidpatternofincreasingstars.java
@@ -0,0 +1,13 @@
+package telegram.day27;
+
+public class Pyramidpatternofincreasingstars {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
