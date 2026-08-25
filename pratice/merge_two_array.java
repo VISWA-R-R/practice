1d8d582df18f5a5c5e409d3557bc2e7e79f4cf72
Viswa R R <viswaoff03@gmail.com>
2026-08-25 17:40:45 +0530

    MatrixwithDiamondPattern

---
+++ pratice/merge_two_array.java
@@ -0,0 +1,11 @@
+package pratice;
+
+import com.sun.tools.javac.Main;
+
+public class merge_two_array {
+    public static void main(String[] args) {
+        int[] arr1={1,2,3,4};
+        int[] arr2={5,6,7,8};
+    }
+    
+}
