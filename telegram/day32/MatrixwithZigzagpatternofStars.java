54400073c3b80e7e219a3b4b8b751e3d505128af
Viswa R R <viswaoff03@gmail.com>
2026-09-08 21:35:43 +0530

    update

--- telegram/day32/MatrixwithZigzagpatternofStars.java
+++ telegram/day32/MatrixwithZigzagpatternofStars.java
@@ -4,11 +4,15 @@ public class MatrixwithZigzagpatternofStars {
     public static void main(String[] args) {
         int n=3;
         for(int i=0;i<n;i++){
-                if(i==0 || i==n-1){
-                    System.out.print("* * *");
-                }
-                else if(i==1){
-                    System.out.print(" * * ");
+                if(i%2==0){
+                    for(int j=0;j<n;j++){
+                        System.out.print("* ");
+                    }
+                }else{
+                    System.out.print(" ");
+                    for(int j=0;j<n-1;j++){
+                        System.out.print("* ");
+                    }
                 }
                 System.out.println();
             }
