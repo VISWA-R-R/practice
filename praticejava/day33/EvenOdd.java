a58ead1e3836f71b57a0e644660e88cca601bc6d
Viswa R R <viswaoff03@gmail.com>
2026-09-09 17:07:45 +0530

    EvenOdd

---
+++ praticejava/day33/EvenOdd.java
@@ -0,0 +1,11 @@
+package praticejava.day33;
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number=4;
+        if(number%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
