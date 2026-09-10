c6259c1c0c730f8af42fc40cb572f1a65713b3a9
Viswa R R <viswaoff03@gmail.com>
2026-09-10 13:08:55 +0530

    BoobleShort

---
+++ praticejava/day34/BoobleShort.java
@@ -0,0 +1,19 @@
+package praticejava.day34;
+
+import java.util.Arrays;
+
+public class BoobleShort {
+    public static void main(String[] args) {
+        int arr[]={7,6,3,4,9,1,2};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
