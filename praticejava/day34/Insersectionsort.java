97507bfb7d628f00dcede02a15041a644bc3359e
Viswa R R <viswaoff03@gmail.com>
2026-09-12 13:01:09 +0530

    Insersectionsort

---
+++ praticejava/day34/Insersectionsort.java
@@ -0,0 +1,19 @@
+package praticejava.day34;
+
+import java.util.Arrays;
+
+public class Insersectionsort {
+    public static void main(String[] args) {
+        int arr[]={5,4,3,2,1};
+        for(int i=1;i<arr.length;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && key<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
