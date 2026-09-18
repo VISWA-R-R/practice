70a7c5d1b360bcd5d42ebdb743b2bed624874ffa
Viswa R R <viswaoff03@gmail.com>
2026-09-18 13:27:39 +0530

    Patternrighttoleft

---
+++ praticejava/day36/Patternrighttoleft.java
@@ -0,0 +1,20 @@
+package praticejava.day36;
+
+public class Patternrighttoleft {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            int k=n-i;
+            for(int j=0;j<n;j++){
+                if(i+j>=n-1){
+                    System.out.print(k);
+                    k++;
+                } 
+                else{
+                    System.out.print(" ");
+                }               
+            }
+            System.out.println();   
+        }
+    }
+}
