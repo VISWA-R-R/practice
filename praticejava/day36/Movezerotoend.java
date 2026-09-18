40daa05483c7ea8ff6aab42353f6a29195411f5d
Viswa R R <viswaoff03@gmail.com>
2026-09-18 12:56:50 +0530

    different method

--- praticejava/day36/Movezerotoend.java
+++ praticejava/day36/Movezerotoend.java
@@ -6,22 +6,36 @@ public class Movezerotoend {
     public static void main(String[] args) {
         int arr[]={1,0,2,0,3,0,0,5,7,8,3,0};
         int i=0;
-        int j=arr.length-1;
-        while(i<j){
-            if(arr[i]==0 && arr[j]!=0){
+        int j=0;
+        while(j<arr.length){
+            if(arr[j]==0){
+                j++;
+            }
+            else{
                 int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
                 i++;
-                j--;
-            }
-            else if(arr[i]!=0){
-                i++;
-            }
-            else if(arr[j]==0){
-                j--;
+                j++;
             }
         }
+
+        // int j=arr.length-1;
+        // while(i<j){
+        //     if(arr[i]==0 && arr[j]!=0){
+        //         int temp=arr[i];
+        //         arr[i]=arr[j];
+        //         arr[j]=temp;
+        //         i++;
+        //         j--;
+        //     }
+        //     else if(arr[i]!=0){
+        //         i++;
+        //     }
+        //     else if(arr[j]==0){
+        //         j--;
+        //     }
+        // }
         System.out.println(Arrays.toString(arr));
     }
 }
