ff415d58bcdd8fee2250da0ff54452a8867d078c
Viswa R R <viswaoff03@gmail.com>
2026-09-22 18:44:31 +0530

    Removeduplicateinarr

--- praticejava/day35/Removeduplicateinarr.java
+++ praticejava/day35/Removeduplicateinarr.java
@@ -2,11 +2,21 @@ package praticejava.day35;
 
 public class Removeduplicateinarr {
     public static void main(String[] args) {
-        int arr[]={1,1,2,2,3,3,3,4};
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length;j++){
-                
+        int arr[]={1,1,1,1,1,1,1,2,2,3,3,3,4};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    for(int k=j;k<n-1;k++){
+                        arr[k]=arr[k+1];
+                    }
+                    n--;
+                    j--;
+                }
             }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]+" ");
         }
     }
 }
