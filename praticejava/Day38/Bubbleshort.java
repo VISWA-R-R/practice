cda46ea8a41f01799acf0cc6f983356a4a0575a9
Viswa R R <viswaoff03@gmail.com>
2026-09-24 18:24:06 +0530

    Bubbleshort

---
+++ praticejava/Day38/Bubbleshort.java
@@ -0,0 +1,19 @@
+package praticejava.Day38;
+
+import java.util.Arrays;
+
+public class Bubbleshort {
+    public static void main(String[] args) {
+        int arr[]={2,3,4,5,1,6,8,9,7};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
