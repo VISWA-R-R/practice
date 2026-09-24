9b26c091941b0c023d093daf477a1efe90d04222
Viswa R R <viswaoff03@gmail.com>
2026-09-24 08:48:32 +0530

    LeapYears

---
+++ telegram2/Day1/LeapYears.java
@@ -0,0 +1,13 @@
+package telegram2.Day1;
+
+public class LeapYears {
+    public static void main(String[] args) {
+        int year=2020;
+        if(year%400==0 || year%4==0 && year%100!=0){
+            System.out.println("it is leap year");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+    }
+}
