230922c1a07e880232ae65efc3432ffb3d35a8cd
Viswa R R <viswaoff03@gmail.com>
2026-09-24 08:41:30 +0530

    Evenodd

---
+++ telegram2/Day1/Evenodd.java
@@ -0,0 +1,13 @@
+package telegram2.Day1;
+
+public class Evenodd {
+    public static void main(String[] args) {
+        int number=4;
+        if(number%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+    }
+}
